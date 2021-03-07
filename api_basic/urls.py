2e0bf91e8b3527f2d4065f3e77d8e5e4eabfb104
iadnanmalik <61354025+iadnanmalik@users.noreply.github.com>
2021-03-07 15:19:50 +0500

    Completed and added authentication
    Completed and added authentication

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, ArticleAPIView,GenericAPIView, ArticleDetails
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>/', article_detail)
+    #path('article/', article_list),
+#    path('detail/<int:pk>/', article_detail)
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+
+
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
 
 ]
